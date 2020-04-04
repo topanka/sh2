395edc2019f3bb4a153dfe780f5c72b72eb1df0a
topanka <33791367+topanka@users.noreply.github.com>
2020-04-04 21:26:32 +0200

    Add files via upload

---
+++ sh2/sh2sh/sh2sh.ino
@@ -0,0 +1,86 @@
+#include <Servo.h> 
+
+#include <uccbst.h>
+#include <sh1tmr.h>
+#include <uccbcrc.h>
+
+#include <RunningAverage.h>
+#include <md18v25.h>
+
+#define SH1_VERSION        "1.0.2"
+
+//battery
+#define UCCB_BATTV_PORT              A1
+#define UCCB_BATTA_PORT              A0
+int g_battV=-1;
+int g_battA=-1;
+
+//temperature
+#define UCCB_TEMPERATURE_PORT       A6
+int g_temperature=-1;
+MYTMR g_tmr_temperature={0};
+
+//general
+unsigned long g_millis=0;
+unsigned long g_loop_cnt=0;         //loop counter
+unsigned long g_loop_cps=0;         //loop counter per sec
+unsigned long g_loop_ct=0;
+int g_recv_ready=0;                 //uccb command received
+
+//UCCB
+
+//battery
+int g_cb_battV=-1;
+
+//joy
+int g_cb_tsX=-1;
+int g_cb_tsY=-1;
+
+int g_cb_fsX=-1;
+int g_cb_fsY=-1;
+int g_cb_fsZ=-1;
+int g_cb_fsBS=-1;
+int g_cb_fsBE=-1;
+
+//10p switch
+int g_cb_sw10p=-1;
+
+//6p button
+int g_cb_b6p=-1;
+int g_cb_b6pBS=-1;
+int g_cb_b6pBE=-1;
+
+//comm
+unsigned long g_cb_w_commpkt_counter=0;
+
+//servo
+int g_cb_tsxp=0;
+int g_cb_tsyp=0;
+
+int g_cb_rdd=0;
+
+//lights
+
+#define SH1_POSLIGHT_PORT        26
+int g_cb_lightpos=UCCB_PL_OFF;
+MYTMR g_tmr_lightpos={0};
+
+//motor
+int g_cb_mls=0;
+int g_cb_m2s=0;
+
+uint16_t g_rpm_m1=0;
+uint16_t g_rpm_m2=0;
+int8_t g_dir_m1=0;
+int8_t g_dir_m2=0;
+
+MYTMR g_tmr_checkmc={0};
+
+//ACS709
+#define SH2SH_ACS709_VIOUTR_PORT              A2    //sensor output
+#define SH2SH_ACS709_VZCRR_PORT               A4    //voltage reference output
+
+#define SH2SH_ACS709_VIOUTL_PORT              A3    //sensor output
+#define SH2SH_ACS709_VZCRL_PORT               A5    //voltage reference output
+
+MYTMR g_tmr_acs709_s1={0};
---
+++ sh2/sh2sh/sh2shacs709.ino
@@ -0,0 +1,91 @@
+int acs709_setup(void)
+{
+  tmr_init(&g_tmr_acs709_s1,50);
+  return(0);
+}
+
+int acs709_read(void)
+{
+  int sol,vzcrl;
+  int sor,vzcrr;
+  int mal,mar;
+
+  if(tmr_do(&g_tmr_acs709_s1) != 1) return(0);
+
+//  Serial.print("*** loop CPS: ");
+//  Serial.println(g_loop_cps);
+  
+  sol=analogRead(SH2SH_ACS709_VIOUTL_PORT);
+  vzcrl=analogRead(SH2SH_ACS709_VZCRL_PORT);
+  sor=analogRead(SH2SH_ACS709_VIOUTR_PORT);
+  vzcrr=analogRead(SH2SH_ACS709_VZCRR_PORT);
+  
+//  if(tmr_do(&g_tmr_acs709_s1) != 1) return(0);
+
+//  Serial.print("*** loop CPS: ");
+//  Serial.println(g_loop_cps);
+
+  Serial.print("VIOUT: ");
+  Serial.print(sol);
+  Serial.print(" ");
+  Serial.println(sor);
+  
+  Serial.print("VZCR: ");
+  Serial.print(vzcrl);
+  Serial.print(" ");
+  Serial.println(vzcrr);
+
+  mal=5000.0*(sol-vzcrl)/1023.0/0.028;
+  mar=5000.0*(sor-vzcrr)/1023.0/0.028;
+
+  Serial.print("mA: ");
+  Serial.print(mal);
+  Serial.print(" ");
+  Serial.println(mar);
+  
+ // Serial.print("Battery voltage: ");
+ // Serial.println(g_battV);
+  
+  return(1);  
+}
+
+int acs709_get_mA(unsigned char viout_pin, unsigned int vzcr, unsigned int *mA)
+{
+  unsigned int viout;
+
+  viout=analogRead(viout_pin);
+//  vzcr=analogRead(vzcr_pin);
+
+  if(viout >= vzcr) {
+    *mA=5000.0*(viout-vzcr)/1023.0/0.028;
+  } else {
+    *mA=0;
+  }
+
+//  Serial.print(viout);
+//  Serial.print(" ");
+
+//if(g_cb_m2s > 0) {
+if(viout_pin == SH2SH_ACS709_VIOUTR_PORT) {
+//  Serial.println();
+  /*
+  Serial.print("VIOUT: ");
+  Serial.print(v1);
+  Serial.print(" ");
+  Serial.print(v2);
+  Serial.print(" ");
+  Serial.print(v3);
+  Serial.print(" ");
+  Serial.print(viout);
+  Serial.print(" ");
+  Serial.println(vzcr);
+  Serial.print("mA=");
+  */
+//  Serial.println(*mA);
+}
+//}
+
+
+//  Serial.print("mA=");
+//  Serial.println(*mA);
+}
