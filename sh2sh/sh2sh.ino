05db27542d9872d9379d89e27a6865e99c2435e8
topanka <33791367+topanka@users.noreply.github.com>
2025-11-23 21:01:02 +0100

    Add files via upload

---
+++ sh2sh/sh2sh.ino
@@ -0,0 +1,107 @@
+#include <Servo.h> 
+
+#include <uccbst.h>
+#include <sh1tmr.h>
+#include <uccbcrc.h>
+
+#include <RunningAverage.h>
+#include <md18v25.h>
+#include <limits.h>
+
+#define SH2_VERSION        "2.0.1"
+
+//battery
+#define UCCB_BATTV_PORT              A1
+#define UCCB_BATTA_PORT              A0
+int g_battV=-1;
+int g_battA=-1;
+unsigned long g_batt_read_tmo=25;
+float g_batt_curr_cutoff=50.0;     //total current cutoff limit in A
+//float g_batt_curr_cutoff=10.0;     //total current cutoff limit in A, testing
+int g_batt_cutoff_reached=0;
+
+//temperature
+#define UCCB_TEMPERATURE_PORT       A6
+int g_temperature=-1;
+MYTMR g_tmr_temperature={0};
+
+//general
+unsigned long g_millis=0;
+unsigned long g_loop_cnt=0;           //loop counter
+unsigned long g_loop_cps=0;           //loop counter per sec
+unsigned long g_loop_ct=0;
+int g_recv_ready=0;                   //uccb command received
+unsigned long g_recv_ready_time=0;    //uccb command received
+
+//UCCB
+
+//battery
+int g_cb_battV=-1;
+
+//joy
+int g_cb_tsX=-1;
+int g_cb_tsY=-1;
+
+int g_cb_fsX=-1;
+int g_cb_fsY=-1;
+int g_cb_fsZ=-1;
+int g_cb_fsBS=-1;
+int g_cb_fsBE=-1;
+
+//10p switch
+int g_cb_sw10p=-1;
+
+//6p button
+int g_cb_b6p=-1;
+int g_cb_b6pBS=-1;
+int g_cb_b6pBE=-1;
+
+//comm
+unsigned long g_cb_w_commpkt_counter=0;
+
+//servo
+int g_cb_tsxp=0;
+int g_cb_tsyp=0;
+
+int g_cb_rdd=0;
+
+//lights
+
+#define SH1_POSLIGHT_PORT        26
+int g_cb_lightpos=UCCB_PL_OFF;
+MYTMR g_tmr_lightpos={0};
+
+//motor
+int g_cb_msl=0;
+int g_cb_msr=0;
+int g_cb_mdreset=0;
+
+uint16_t g_rpm_ml=0;
+uint16_t g_rpm_mr=0;
+int8_t g_dir_ml=0;
+int8_t g_dir_mr=0;
+uint8_t g_state_ml=0;
+uint8_t g_state_mr=0;
+
+MYTMR g_tmr_checkmc={0};
+
+unsigned int g_md_curr_cutoff=25000;          //md current cutoff limit in mA
+//unsigned int g_md_curr_cutoff=6000;           //md current cutoff limit in mA, testing
+
+int g_cb_md_stop=0;
+
+//ACS709
+#define SH2SH_ACS709_VIOUTR_PORT              A2    //sensor output
+#define SH2SH_ACS709_VZCRR_PORT               A4    //voltage reference output
+
+#define SH2SH_ACS709_VIOUTL_PORT              A3    //sensor output
+#define SH2SH_ACS709_VZCRL_PORT               A5    //voltage reference output
+
+MYTMR g_tmr_acs709_s1={0};
+
+//ACS770LCB-100U-PFF
+float g_ACS770_FACTOR=(40.0/1000.0);
+float g_ACS770_QOV=0.5;
+float g_ACS770_cutoff=((g_ACS770_FACTOR)*g_batt_curr_cutoff);
+
+//1-left, 2-right
---
+++ sh2sh/sh2shbatt.ino
@@ -0,0 +1,126 @@
+RunningAverage ra_bv(5);
+RunningAverage ra_ba(7);
+
+int batt_setup(void)
+{
+  int i,v,avn;
+  
+  avn=7;
+  
+//fill the buffer  
+  for(i=0;i < avn;i++) {
+    v=analogRead(UCCB_BATTV_PORT);
+    ra_bv.addValue(v);
+  }
+  
+  avn=11;
+  
+//fill the buffer  
+  for(i=0;i < avn;i++) {
+    v=analogRead(UCCB_BATTA_PORT);
+    ra_ba.addValue(v);
+  }
+  
+  return(0);
+}
+
+int batt_read(int *battV, int *battA)
+{
+  static unsigned long l_brtv=0,l_brta=0;
+  static int l_bv=0;
+  static int l_ba=0;
+  long a;
+
+  if((g_millis-l_brtv) >= 500) {
+    l_bv=analogRead(UCCB_BATTV_PORT);
+    ra_bv.addValue(l_bv);
+    l_bv=round(ra_bv.getAverage()+0.5);
+    l_brtv=g_millis;
+  }
+  if((g_millis-l_brta) >= g_batt_read_tmo) {
+    l_ba=analogRead(UCCB_BATTA_PORT);
+    if(l_ba >= 100) {
+      a=ra_ba.getAverage();
+      if(l_ba > a+10) {
+        l_ba=a+10;
+      } else if(l_ba < a-10) {
+        if(a > 10) {
+          l_ba=a-10;
+        }
+      }
+      ra_ba.addValue(l_ba);
+    } else {
+      ra_ba.addValue(100);
+    }
+    l_ba=round(ra_ba.getAverage()+0.5);
+    l_brta=g_millis;
+    batt_curr_cutoff(l_ba);
+  }
+  
+  *battV=l_bv;
+  *battA=l_ba;
+  
+  return(0);
+}
+
+
+void batt_curr_cutoff(int ba)
+{
+  float voltage;
+  static int l_limit=0;
+
+  voltage=(5.0/1023.0)*ba;
+  voltage=voltage-g_ACS770_QOV+0.015;
+
+//  Serial.print("*** batt amp: ");
+//  Serial.println(voltage);
+
+  if(voltage > g_ACS770_cutoff) {
+    Serial.print("*** batt cutOff1: ");
+//    Serial.println(voltage);
+    Serial.println(25*voltage);
+    if(l_limit < 3) {
+      l_limit++;
+    }
+    g_batt_read_tmo=10;
+  } else {
+    if(l_limit < 3) {
+      l_limit=0;
+    }
+    if(g_cb_md_stop == 1) {
+      l_limit=0;
+      g_batt_read_tmo=25;
+      g_batt_cutoff_reached=0;
+    }
+  }
+
+  if((l_limit >= 3) && (g_batt_cutoff_reached != 1)) {
+    g_batt_cutoff_reached=1;
+    Serial.print("*** batt cutOff: ");
+//    Serial.print(g_ACS770_cutoff);
+    Serial.print(25*g_ACS770_cutoff);
+    Serial.println(" reached");
+  }
+}
+
+void totcurr(void)
+{
+  float voltage;
+  float current;
+
+  voltage=(5.0/1023.0)*g_battA;
+  voltage=voltage-g_ACS770_QOV+0.015;
+  current=voltage/g_ACS770_FACTOR;
+
+  Serial.print("totA:");
+  Serial.print(g_battA);
+  Serial.print(" ");
+  Serial.print(voltage);
+  Serial.print(" ");
+  Serial.print(current);
+  Serial.print(" ");
+  Serial.print(g_ACS770_cutoff);
+  Serial.print(" ");
+  Serial.println();
+  
+}
